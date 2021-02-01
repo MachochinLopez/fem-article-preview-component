7235b4281a59822c7391f0a08c9f7fbf65fa5fd8
Oscar López <machochinnio@hotmail.com>
2021-01-31 23:31:14 -0600

    Release version 1

--- src/js/open_share_section.js
+++ src/js/open_share_section.js
@@ -6,9 +6,11 @@ for (const shareButton of shareButtons) {
     // Quita la clase que oculta el share section.
     const shareOptionsContainer = this.previousElementSibling;
     shareOptionsContainer.classList.toggle("article-preview__share-options-container--hidden");
+    shareOptionsContainer.classList.add("article-preview__share-options-container--animated");
     // Oculta la sección del autor.
     const authorContainer = shareOptionsContainer.previousElementSibling;
     authorContainer.classList.toggle("article-preview__author-info-container--hidden");
+    authorContainer.classList.add("article-preview__author-info-container--animated");
     // Colorea el share button y el icon.
     this.classList.toggle("article-preview__share-button--colorized");
   });
